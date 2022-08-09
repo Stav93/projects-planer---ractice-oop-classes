075fa66559ef36e8707dff50090929dba52bdbbe
Stav <stavush93@gmail.com>
2022-08-09 15:31:46 +0300

    starting the switch logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,21 @@
 class Tooltip { }
 
 class ProjectItem {
-  constructor(id) {
+  constructor(id, updateProjectListFunc) {
     this.id = id;
-    this.moreInfobtn();
-    this.switchBtn();
+    this.updateProjectListFunc = updateProjectListFunc;
+    this.moreInfobtnFunc();
+    this.switchBtnFunc();
   }
 
-  switchBtn() {
+  moreInfobtnFunc() {
 
   }
-
-  moreInfobtn() {
-    
+  
+  switchBtnFunc() {
+    const projectItemEl = document.getElementById(this.id);
+    const switchBtn = projectItemEl.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click", this.updateProjectListFunc);
   }
 
 }
@@ -21,11 +24,29 @@ class ProjectList {
   projects = [];
 
   constructor(type) {
+    this.type = type;
+
     const prjItems = document.querySelectorAll(`#${type}-projects li`)
     for (const project of prjItems) {
-      this.projects.push(new ProjectItem(project.id))
+      this.projects.push(new ProjectItem(project.id, this.switchProject.bind(this)))
     }
     console.log(this.projects)
+  }
+
+  // we have to call it after the construter created the projects objects
+  setSwitchHaandlerFunc(switchHaandlerFunc) {
+    this.switchHaandlerFunc = switchHaandlerFunc;
+  }
+
+  addProjectToSection() {
+    console.log(this)
+  }
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex(p => p.id === projectId)
+    // this.projects.splice(projectIndex, 1)
+    this.switchHaandlerFunc(this.projects.find(p => p.id === projectId))
+    this.projects = this.projects.filter(p => p.id !== projectId)
   }
 }
 
@@ -34,6 +55,10 @@ class App {
   static init() {
     const activeProjectsList = new ProjectList("active");
     const finishedProjectsList = new ProjectList("finished");
+
+    // changing the instance - the "this"
+    activeProjectsList.setSwitchHaandlerFunc(finishedProjectsList.addProjectToSection.bind(finishedProjectsList))
+    finishedProjectsList.setSwitchHaandlerFunc(activeProjectsList.addProjectToSection.bind(activeProjectsList))
   }
 }
 
