21182b13b297b9af25b8c68a4892439dad989749
Stav <stavush93@gmail.com>
2022-08-12 15:48:56 +0300

    change the style of the section while dragged

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -162,6 +162,27 @@ class ProjectList {
       );
     }
     console.log(this.projects);
+    this.connectDroppavle();
+  }
+
+  connectDroppavle() {
+    const list = document.querySelector(`#${this.type}-projects ul`);
+    list.addEventListener("dragenter", event => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        list.parentElement.classList.add("droppable")
+        event.preventDefault();
+      }
+    });
+    list.addEventListener("dragover", event => {
+      if (event.dataTransfer.types[0] === "text/plain") {
+        event.preventDefault();
+      }
+    });
+    list.addEventListener("dragleave", event => {
+      if (event.relatedTarget.closest(`#${this.type}-projects ul`) !== list) {
+        list.parentElement.classList.remove("droppable")
+      }
+    });
   }
 
   // we have to call it after the construter created the projects objects
